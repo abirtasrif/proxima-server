708c00bcb6a176eacb6bac2e7b2706804ff33bfb
Abir Tasrif Anto <abirtasrif@gmail.com>
2023-02-12 23:29:37 +0600

    roter setup

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 require("dotenv").config();
-const express = require("express");
 
-//express app
+const express = require("express");
+const projectRoutes = require("./routes/projectRoute");
+
+// express app
 const app = express();
 
-//port
+// port
 const port = process.env.PORT || 4000;
 
-//listen for requests
+// middlewares
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/api/projects", projectRoutes);
+
+// listen for requests
 app.listen(port, () => {
-  console.log(`listening port ${port}`);
+  console.log(`listening on port ${port}`);
 });

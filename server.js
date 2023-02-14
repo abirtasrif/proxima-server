8875dab19af6b02acad8831521307b55cbf38f5f
Abir Tasrif Anto <abirtasrif@gmail.com>
2023-02-14 23:05:56 +0600

    adding cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const projectRoutes = require("./routes/projectRoute");
+const cors = require("cors");
 
 // express app
 const app = express();
@@ -11,6 +12,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // middlewares
+app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
